b810caa430f8613316cb51e7c13a0d2368bd2f84
Ajay yadav <majaycool5@gmail.com>
2023-11-16 19:57:09 +0530

    added component Hero

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import LoginSignup from './Pages/LoginSignup';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import './Navbar.css';
 
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -20,14 +21,18 @@ const Navbar = () => {
             {/* {menuItems.map((item) =>(
                 <li key={item} onClick={() => setMenu(item)}>{item}{menu === item ? <hr/> : <></>}</li>
             ))} */}
-            <li onClick={() => {setMenu("Shop")}}>Shop{menu==="Shop"?<hr/>:<></>}</li>
-            <li onClick={() => {setMenu("Men")}}>Men{menu==="Men"?<hr/>:<></>}</li>
-            <li onClick={() => {setMenu("Women")}}>Women{menu==="Women"?<hr/>:<></>}</li>
-            <li onClick={() => {setMenu("Kids")}}>Kids{menu==="Kids"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu("Shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="Shop"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu("Men")}}><Link style={{textDecoration:'none'}} to='/mens'>Men</Link>{menu==="Men"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu("Women")}}><Link style={{textDecoration:'none'}} to='/Womens'>Women</Link>{menu==="Women"?<hr/>:<></>}</li>
+            <li onClick={() => {setMenu("Kids")}}><Link style={{textDecoration:'none'}} to='/kids'>Kids</Link>{menu==="Kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
+            <Link to='/login'>
             <button>Login</button>
+            </Link>
+            <Link to='cart'>
             <img src={cart_icon} alt="" />
+                 </Link>
             <div className="nav-cart-count">0</div>
         </div>
     </div>
